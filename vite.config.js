3fb5ccfc349f361f631bfca8a94f9e9a1c4b0454
Bibek Rimal <info@bibekrimal.com.np>
2025-08-25 14:58:01 +0100

    new build for prod

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,7 @@ import { dirname, resolve } from "path";
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
 export default defineConfig({
+  base: "/cubscouts/",
   plugins: [tailwindcss()],
   build: {
     outDir: "build",
